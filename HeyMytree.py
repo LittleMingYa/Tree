f66e1148c36dae0f6253304d246dc2a3c5671b19
LittleMingYa <48229768+LittleMingYa@users.noreply.github.com>
2019-03-13 20:03:39 +0800

    Create HeyMytree.py

---
+++ HeyMytree.py
@@ -0,0 +1,27 @@
+import turtle
+
+def draw_branch(branch_length):
+    if branch_length > 22:
+        turtle.forward(branch_length)
+        turtle.right(20)
+        draw_branch(branch_length - 7)
+
+        turtle.left(40)
+        draw_branch(branch_length - 7)
+
+        turtle.right(20)
+        turtle.backward(branch_length)
+
+
+def main():
+    turtle.left(90)
+    turtle.penup()
+    turtle.backward(120)
+    turtle.pendown()
+    turtle.pensize(7)
+    turtle.pencolor('darkgreen')
+    draw_branch(77)
+    turtle.exitonclick()
+
+if __name__ == '__main__':
+    main()
